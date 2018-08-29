2bb008a5d4a0d99c89d188d68e78919a312371c0
Minh N. Bui <buinhatminh1995@gmail.com>
2018-08-29 16:51:18 -0700

    Reorganize scripts folders

--- Scripts/SelectTopLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Helper script for selecting top-level parent in Unity Editor Scene window
-// attach to the top-most game object to select it instead of a random child component when selecting inside the Scene window.
-
-[SelectionBase]
-public class SelectTopLevel : MonoBehaviour {}
